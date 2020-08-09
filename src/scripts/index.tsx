eae530ed5917b8a3dac5d9ab29aba136909c89f7
Erick Christian <erickchristian48@gmail.com>
2020-08-09 23:31:51 +0700

    Added initialization for firebase and react

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,1 +1,19 @@
 import * as React from 'react'
+import { render } from 'react-dom'
+
+import * as firebase from 'firebase/app'
+import { firebaseConfig } from './firebaseConfig'
+
+firebase.initializeApp(firebaseConfig)
+
+// Check if we're in dev server
+// If yes, use firebase emulator
+if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
+  firebase.firestore().settings({
+    host: 'localhost:8080',
+    ssl: false,
+  })
+  firebase.functions().useFunctionsEmulator('http://localhost:5001')
+}
+
+render(<React.StrictMode></React.StrictMode>, document.getElementById('root'))
