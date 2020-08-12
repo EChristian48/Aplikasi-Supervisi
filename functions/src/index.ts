2a0e909f82fd49ae994d01644d1e666731eb5c92
Erick Christian <erickchristian48@gmail.com>
2020-08-12 10:58:31 +0700

    Now selected only a few props

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,13 +1,21 @@
 import * as functions from 'firebase-functions'
 import * as admin from 'firebase-admin'
 
+import { User } from './dataSchema'
+
 admin.initializeApp()
 
 const createUserDocument = functions.auth.user().onCreate(user => {
   const userDocRef = admin.firestore().doc(`users/${user.uid}`)
-  const userData = { ...user }
 
-  return userDocRef.create(userData)
+  const userData: User = {
+    uid: user.uid,
+    email: user.email,
+    name: user.displayName,
+    photoUrl: user.photoURL,
+  }
+
+  userDocRef.create(userData)
 })
 
 export { createUserDocument }
