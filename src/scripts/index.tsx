d61ca46ca0b7db8c68d14406440e186b228fa834
Erick Christian <erickchristian48@gmail.com>
2020-08-13 10:04:45 +0700

    Reordered imports

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -1,23 +1,24 @@
-import '../styles/style.css'
-
-import * as React from 'react'
-import { render } from 'react-dom'
-
 import * as firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/functions'
 import { firebaseConfig } from './firebaseConfig'
-import { App } from './App'
 
 firebase.initializeApp(firebaseConfig)
+
+import * as React from 'react'
+import { render } from 'react-dom'
+
+import { App } from './App'
+
+import '../styles/style.css'
 
 // Check if we're in dev server
 // If yes, use firebase emulator
 if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
-  firebase.firestore().settings({
-    host: 'localhost:8080',
-    ssl: false,
-  })
+  // firebase.firestore().settings({
+  //   host: 'localhost:8080',
+  //   ssl: false,
+  // })
   firebase.functions().useFunctionsEmulator('http://localhost:5001')
 }
 
