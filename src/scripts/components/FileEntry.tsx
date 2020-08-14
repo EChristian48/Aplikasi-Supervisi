e9acb3ad6527fc9c2602145eee8303302d39c8a2
Erick Christian <erickchristian48@gmail.com>
2020-08-14 16:03:27 +0700

    Created FileEntry for DocTable

---
+++ src/scripts/components/FileEntry.tsx
@@ -0,0 +1,42 @@
+import * as React from 'react'
+import {
+  TableRow,
+  TableCell,
+  Button,
+  Typography,
+  Link,
+} from '@material-ui/core'
+
+type FileEntryProps = {
+  file: firebase.storage.Reference
+  deleteCallback: () => any
+}
+
+const FileEntry: React.FC<FileEntryProps> = props => {
+  const [urlString, setUrlString] = React.useState<string>()
+  props.file.getDownloadURL().then(result => setUrlString(result))
+
+  return (
+    <TableRow>
+      <TableCell>
+        <Typography>
+          {urlString ? (
+            <Link href={urlString}>{props.file.name}</Link>
+          ) : (
+            `${props.file.name} (Loading link...)`
+          )}
+        </Typography>
+      </TableCell>
+      <TableCell>
+        <Button
+          onClick={() => {
+            props.file.delete().then(props.deleteCallback)
+          }}>
+          Delete
+        </Button>
+      </TableCell>
+    </TableRow>
+  )
+}
+
+export { FileEntry }
