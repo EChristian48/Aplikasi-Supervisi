f78216f54ad6d606301c3af7135d4eea74919313
Erick Christian <erickchristian48@gmail.com>
2020-08-15 08:27:44 +0700

    Created

---
+++ src/scripts/pages/supervisor/SuperDocs.tsx
@@ -0,0 +1,71 @@
+import * as React from 'react'
+
+import * as firebase from 'firebase/app'
+import 'firebase/firestore'
+
+import {
+  Container,
+  Grid,
+  List,
+  ListItemText,
+  ListItem,
+  TableCell,
+  TableRow,
+} from '@material-ui/core'
+import { DocTable } from '../../components/DocTable'
+import { User } from '../../dataSchema'
+import { Switch, Route, useRouteMatch } from 'react-router-dom'
+import { SpecGuruDocs } from './SpecGuruDocs'
+
+const SuperDocs: React.FC = () => {
+  const [gurus, setGurus] = React.useState<User[]>()
+  const match = useRouteMatch<{ id: string }>()
+
+  React.useEffect(() => {
+    firebase
+      .firestore()
+      .collection('users')
+      .where('role', '==', 'guru')
+      .get()
+      .then(result => {
+        const gurus: User[] = []
+        result.forEach(doc => {
+          gurus.push(doc.data() as User)
+        })
+        setGurus(gurus)
+      })
+  }, [])
+
+  return (
+    <Container>
+      <Grid container>
+        <Switch>
+          <Route path={`${match.path}/:id`}>
+            <SpecGuruDocs />
+          </Route>
+
+          <Route path={`${match.path}`}>
+            <Grid item xs={12}>
+              <DocTable headers={['Nama Guru', 'Mata Pelajaran', 'E-Mail']}>
+                {gurus?.map(guru => (
+                  <TableRow
+                    key={guru.uid}
+                    hover
+                    onClick={() =>
+                      (location.href = `#${match.path}/${guru.uid}`)
+                    }>
+                    <TableCell>{guru.name}</TableCell>
+                    <TableCell>{guru.uid}</TableCell>
+                    <TableCell>{guru.email}</TableCell>
+                  </TableRow>
+                ))}
+              </DocTable>
+            </Grid>
+          </Route>
+        </Switch>
+      </Grid>
+    </Container>
+  )
+}
+
+export { SuperDocs }
