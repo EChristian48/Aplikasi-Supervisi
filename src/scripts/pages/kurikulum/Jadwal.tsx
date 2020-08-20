861c4db0f9bb0aa1eea31f6440580708fa1915f3
Erick Christian <erickchristian48@gmail.com>
2020-08-20 15:31:00 +0700

    Added xlsx parsing and saving to database

--- src/scripts/pages/kurikulum/Jadwal.tsx
+++ src/scripts/pages/kurikulum/Jadwal.tsx
@@ -10,23 +10,66 @@ import {
   Typography,
   Button,
 } from '@material-ui/core'
+import { read, utils } from 'xlsx'
+import HtmlParser from 'react-html-parser'
+import * as firebase from 'firebase/app'
+
+import { Jadwal } from '../../dataSchema'
 
 const days: string[] = ['Senin', 'Selasa', 'Rabu', 'Kamis', 'Jumat', 'Sabtu']
 
 const Jadwal: React.FC = () => {
-  const [hari, setHari] = React.useState()
+  const [hari, setHari] = React.useState('')
+  const [table, setTable] = React.useState('')
+
+  React.useEffect(() => {
+    if (hari) {
+      firebase
+        .firestore()
+        .collection('jadwal')
+        .doc(hari)
+        .get()
+        .then(result => {
+          const data = result.data() as Jadwal
+          if (data) {
+            setTable(data.html)
+            console.log('data diambli')
+          } else {
+            setTable('')
+          }
+        })
+    }
+  }, [hari])
 
   const handleHari = (e: React.ChangeEvent<{ value: string }>) => {
-    console.log(e.target.value)
+    setHari(e.target.value)
+  }
+
+  const handleFile = (e: React.ChangeEvent<HTMLInputElement>) => {
+    if (e.target.files !== null) {
+      const file = e.target.files[0]
+      const reader = new FileReader()
+
+      reader.onload = e => {
+        if (e.target !== null) {
+          const data = new Uint8Array(e.target.result as ArrayBuffer)
+          const workbook = read(data, { type: 'array' })
+
+          setTable(utils.sheet_to_html(workbook.Sheets[workbook.SheetNames[0]]))
+        }
+      }
+
+      reader.readAsArrayBuffer(file)
+    }
   }
 
   return (
-    <Container className='mt-2'>
+    <Container>
       <Grid container>
-        <Grid item xs={6}>
+        <Grid item lg={2} xs={12}>
           <FormControl style={{ minWidth: 120 }}>
             <InputLabel id='hari'>Pilih Hari</InputLabel>
-            <Select labelId='hari' onChange={handleHari}>
+            <Select labelId='hari' onChange={handleHari} value={hari}>
               {days.map(day => (
                 <MenuItem key={day} value={day}>
                   {day}
@@ -35,17 +78,36 @@ const Jadwal: React.FC = () => {
             </Select>
           </FormControl>
         </Grid>
-        <Grid item xs={6}>
-          <Typography>
-            Upload XLSX: <input type='file' name='' id='' />
-          </Typography>
-          <Button>Kirim</Button>
-        </Grid>
-      </Grid>
-      <Grid container>
-        <Grid item xs={12}>
-          {/* TODO: Kasih XLSX */}
-          <Typography>Belom beres gan</Typography>
+        <Grid container item xs={12} lg={10}>
+          <Grid item xs={12} style={{ marginTop: 8 }}>
+            <Typography>
+              <input
+                type='file'
+                name='jadwalInput'
+                id='jadwalInput'
+                onChange={handleFile}
+                disabled={!!!hari}
+              />
+            </Typography>
+          </Grid>
+
+          <Grid item xs={12}>
+            {HtmlParser(table)}
+          </Grid>
+
+          <Grid item xs={12} style={{ marginTop: 8 }}>
+            <Button
+              fullWidth
+              variant='outlined'
+              disabled={!!!hari}
+              onClick={() => {
+                firebase.firestore().collection('jadwal').doc(hari).set({
+                  html: table,
+                })
+              }}>
+              Simpan
+            </Button>
+          </Grid>
         </Grid>
       </Grid>
     </Container>
