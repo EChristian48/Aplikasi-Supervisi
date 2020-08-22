bdd982672fdf9db6c7b30e6743041853a2f3027e
Erick Christian <erickchristian48@gmail.com>
2020-08-22 15:58:43 +0700

    Tidied up return

--- src/scripts/components/LoadingBanner.tsx
+++ src/scripts/components/LoadingBanner.tsx
@@ -2,12 +2,10 @@ import * as React from 'react'
 
 import { Backdrop, CircularProgress, BackdropProps } from '@material-ui/core'
 
-const LoadingBanner: React.FC<BackdropProps> = props => {
-  return (
-    <Backdrop {...props}>
-      <CircularProgress />
-    </Backdrop>
-  )
-}
+const LoadingBanner: React.FC<BackdropProps> = props => (
+  <Backdrop {...props}>
+    <CircularProgress />
+  </Backdrop>
+)
 
 export { LoadingBanner }
