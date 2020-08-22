95f11874ec14e8b9997f5aa2d1a378a97936b3d0
Erick Christian <erickchristian48@gmail.com>
2020-08-22 14:29:07 +0700

    Moved menus to a separate file

---
+++ src/scripts/data/menus.tsx
@@ -0,0 +1,39 @@
+import * as React from 'react'
+import { Role } from '../dataSchema'
+import { Description, Schedule } from '@material-ui/icons'
+
+type Menu = {
+  icon: JSX.Element
+  name: string
+  role: Role
+  link: string
+}
+
+const menus: Menu[] = [
+  {
+    icon: <Description />,
+    name: 'Dokumen Pembelajaran',
+    role: 'guru',
+    link: '#/guru/upload',
+  },
+  {
+    icon: <Description />,
+    name: 'Daftar Dokumen',
+    role: 'supervisor',
+    link: '#/supervisor/dokumen',
+  },
+  {
+    icon: <Schedule />,
+    name: 'Daftar Jadwal',
+    role: 'kurikulum',
+    link: '#/kurikulum/jadwal',
+  },
+  {
+    icon: <Description />,
+    name: 'Lihat Laporan',
+    role: 'kepsek',
+    link: '#/kepsek/laporan',
+  },
+]
+
+export { menus }
