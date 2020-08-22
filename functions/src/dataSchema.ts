13ebc8db2ec44fe80e67206366b23159f4b0badc
Erick Christian <erickchristian48@gmail.com>
2020-08-22 19:36:28 +0700

    Updated to match the client version

--- functions/src/dataSchema.ts
+++ functions/src/dataSchema.ts
@@ -4,11 +4,14 @@ type User = {
   email: string | undefined | null
   photoUrl: string | undefined | null
   role: Role
+  mapel?: string
 }
 
 type Role = 'guru' | 'supervisor' | 'kurikulum' | 'kepsek' | undefined
 
 type GuruFile = {
+  uploader: string | undefined | null
+  uploaderName: string | undefined | null
   name: string
   downloadUrl: string
   fullPath: string
