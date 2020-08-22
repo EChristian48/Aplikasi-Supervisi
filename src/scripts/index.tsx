776196039bc558e52337af8c0031a78272040659
Erick Christian <erickchristian48@gmail.com>
2020-08-22 13:13:04 +0700

    Removed Emulator config

--- src/scripts/index.tsx
+++ src/scripts/index.tsx
@@ -14,16 +14,6 @@ import { App } from './App'
 
 import '../styles/style.css'
 
-// Check if we're in dev server
-// If yes, use firebase emulator
-if (location.hostname === '127.0.0.1' || location.hostname === 'localhost') {
-  // firebase.firestore().settings({
-  //   host: 'localhost:8080',
-  //   ssl: false,
-  // })
-  firebase.functions().useFunctionsEmulator('http://localhost:5001')
-}
-
 render(
   <React.StrictMode>
     <App />
