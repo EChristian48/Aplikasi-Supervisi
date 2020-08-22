e45714c03eb0c6f7658cd456c8d1e54ec0ec5357
Erick Christian <erickchristian48@gmail.com>
2020-08-22 19:36:45 +0700

    Added loading for status and deletion

--- src/scripts/components/FileEntry.tsx
+++ src/scripts/components/FileEntry.tsx
@@ -5,8 +5,11 @@ import {
   Button,
   Typography,
   Link,
+  CircularProgress,
 } from '@material-ui/core'
 import { auth, storage, firestore } from 'firebase/app'
+import { getFirestorePath } from './UploadDialog'
+import { GuruFile } from '../dataSchema'
 
 type FileEntryProps = {
   file: storage.Reference
@@ -15,21 +18,38 @@ type FileEntryProps = {
 
 const FileEntry: React.FC<FileEntryProps> = props => {
   const [urlString, setUrlString] = React.useState<string>()
+  const [status, setStatus] = React.useState('Loading...')
+  const [isDeleting, setDeleting] = React.useState(false)
 
   React.useEffect(() => {
     props.file.getDownloadURL().then(result => {
       setUrlString(result)
     })
+
+    getFile().then(doc => {
+      const data = doc.data() as GuruFile
+      const status = data.accepted ? 'Diterima' : 'Belum Diterima'
+      setStatus(status)
+    })
   }, [])
 
+  const getFile = () =>
+    firestore()
+      .collection('guru')
+      .doc(auth().currentUser?.uid)
+      .collection('files')
+      .doc(getFirestorePath(props.file))
+      .get()
+
   const deleteFile = () => {
+    setDeleting(true)
     return Promise.all([
       props.file.delete(),
       firestore()
         .collection('guru')
         .doc(auth().currentUser?.uid)
         .collection('files')
-        .doc(props.file.fullPath.split('/').join())
+        .doc(getFirestorePath(props.file))
         .delete(),
     ])
   }
@@ -45,10 +65,15 @@ const FileEntry: React.FC<FileEntryProps> = props => {
           )}
         </Typography>
       </TableCell>
+      <TableCell>{status}</TableCell>
       <TableCell>
-        <Button onClick={() => deleteFile().then(props.deleteCallback)}>
-          Delete
-        </Button>
+        {isDeleting ? (
+          <CircularProgress variant='indeterminate' />
+        ) : (
+          <Button onClick={() => deleteFile().then(props.deleteCallback)}>
+            Delete
+          </Button>
+        )}
       </TableCell>
     </TableRow>
   )
