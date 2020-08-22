52d0321853cb460939740818e1671d71ef5b72cd
Erick Christian <erickchristian48@gmail.com>
2020-08-22 15:58:30 +0700

    Created CustomDrawer from SwipableDrawer

---
+++ src/scripts/components/CustomDrawer.tsx
@@ -0,0 +1,33 @@
+import * as React from 'react'
+import { SwipeableDrawer, Divider, List } from '@material-ui/core'
+import { DrawerItem } from './MenuItem'
+import { PowerSettingsNew } from '@material-ui/icons'
+
+type CustomDrawerProps = {
+  open: boolean
+  onClose: () => void
+  onOpen: () => void
+  logout: () => void | Promise<void>
+}
+
+const CustomDrawer: React.FC<CustomDrawerProps> = props => {
+  return (
+    <SwipeableDrawer
+      open={props.open}
+      onClose={props.onClose}
+      onOpen={props.onOpen}
+      anchor='left'>
+      {props.children}
+
+      <Divider />
+
+      <List>
+        <DrawerItem icon={<PowerSettingsNew />} onClick={props.logout}>
+          Logout
+        </DrawerItem>
+      </List>
+    </SwipeableDrawer>
+  )
+}
+
+export { CustomDrawer }
