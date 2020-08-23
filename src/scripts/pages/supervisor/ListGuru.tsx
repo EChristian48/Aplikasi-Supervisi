2a8809bcb597c1f5bbd5245a5bc500c097220dae
Erick Christian <erickchristian48@gmail.com>
2020-08-23 18:29:09 +0700

    Added LoadingBanner

--- src/scripts/pages/supervisor/ListGuru.tsx
+++ src/scripts/pages/supervisor/ListGuru.tsx
@@ -7,9 +7,11 @@ import { BasicTable } from '../../components/BasicTable'
 import { User } from '../../dataSchema'
 import { Switch, Route, useRouteMatch } from 'react-router-dom'
 import { ListDokumen } from './ListDokumen'
+import { LoadingBanner } from '../../components/LoadingBanner'
 
 const ListGuru: React.FC = () => {
   const [gurus, setGurus] = React.useState<User[]>([])
+  const [isLoading, setLoading] = React.useState(true)
   const match = useRouteMatch<{ id: string }>()
 
   const getGurus = () =>
@@ -17,6 +19,7 @@ const ListGuru: React.FC = () => {
 
   React.useEffect(() => {
     getGurus().then(result => {
+      setLoading(false)
       const gurus: User[] = []
       result.forEach(doc => {
         gurus.push(doc.data() as User)
@@ -26,34 +29,38 @@ const ListGuru: React.FC = () => {
   }, [])
 
   return (
-    <Container>
-      <Grid container>
-        <Switch>
-          <Route path={`${match.path}/:id`}>
-            <ListDokumen />
-          </Route>
+    <>
+      {' '}
+      <Container>
+        <Grid container>
+          <Switch>
+            <Route path={`${match.path}/:id`}>
+              <ListDokumen />
+            </Route>
 
-          <Route path={`${match.path}`}>
-            <Grid item xs={12}>
-              <BasicTable headers={['Nama Guru', 'Mata Pelajaran', 'E-Mail']}>
-                {gurus.map(guru => (
-                  <TableRow
-                    key={guru.uid}
-                    hover
-                    onClick={() =>
-                      (location.href = `#${match.path}/${guru.uid}`)
-                    }>
-                    <TableCell>{guru.name}</TableCell>
-                    <TableCell>{guru.mapel}</TableCell>
-                    <TableCell>{guru.email}</TableCell>
-                  </TableRow>
-                ))}
-              </BasicTable>
-            </Grid>
-          </Route>
-        </Switch>
-      </Grid>
-    </Container>
+            <Route path={`${match.path}`}>
+              <Grid item xs={12}>
+                <BasicTable headers={['Nama Guru', 'Mata Pelajaran', 'E-Mail']}>
+                  {gurus.map(guru => (
+                    <TableRow
+                      key={guru.uid}
+                      hover
+                      onClick={() =>
+                        (location.href = `#${match.path}/${guru.uid}`)
+                      }>
+                      <TableCell>{guru.name}</TableCell>
+                      <TableCell>{guru.mapel}</TableCell>
+                      <TableCell>{guru.email}</TableCell>
+                    </TableRow>
+                  ))}
+                </BasicTable>
+              </Grid>
+            </Route>
+          </Switch>
+        </Grid>
+      </Container>
+      <LoadingBanner open={isLoading} />
+    </>
   )
 }
 
